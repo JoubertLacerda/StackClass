6af844ebac3d2ef3fa8f4dd364181d65e4f5268a
Joubert Lacerda <46083794+JoubertLacerda@users.noreply.github.com>
2019-03-10 15:21:06 +1000

    Add files via upload

---
+++ Stacks/Stacks/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Stacks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtOUTPUT = new System.Windows.Forms.TextBox();
+            this.btnEXIT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(12, 74);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "&Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtOUTPUT
+            // 
+            this.txtOUTPUT.Location = new System.Drawing.Point(129, 44);
+            this.txtOUTPUT.Multiline = true;
+            this.txtOUTPUT.Name = "txtOUTPUT";
+            this.txtOUTPUT.Size = new System.Drawing.Size(315, 265);
+            this.txtOUTPUT.TabIndex = 1;
+            // 
+            // btnEXIT
+            // 
+            this.btnEXIT.Location = new System.Drawing.Point(485, 331);
+            this.btnEXIT.Name = "btnEXIT";
+            this.btnEXIT.Size = new System.Drawing.Size(75, 23);
+            this.btnEXIT.TabIndex = 2;
+            this.btnEXIT.Text = "E&xit";
+            this.btnEXIT.UseVisualStyleBackColor = true;
+            this.btnEXIT.Click += new System.EventHandler(this.btnEXIT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 366);
+            this.Controls.Add(this.btnEXIT);
+            this.Controls.Add(this.txtOUTPUT);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtOUTPUT;
+        private System.Windows.Forms.Button btnEXIT;
+    }
+}
+
---
+++ Stacks/Stacks/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Stacks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            //create a stack
+            Stack months = new Stack();
+
+            //Insert elemets to the stack 
+            months.Push("January");
+            months.Push("Feb");
+            months.Push("March");
+            months.Push("April");
+            months.Push("May");
+            months.Push("June");
+            months.Push("July");
+            months.Push("August");
+            months.Push("Sept");
+            months.Push("October");
+            months.Push("November");
+            months.Push("Dec");
+
+            //Returns number of elements in stack
+            txtOUTPUT.Text += "Num. of elements in the stack:  " + months.Count.ToString();
+
+            //return the top most of elemet of stack
+            txtOUTPUT.Text += "\r\nThe top most item of stack is: "+months.Peek().ToString();
+
+            //view items in the stack.
+            txtOUTPUT.Text += "\r\nItems in the Stack: \r\n";
+            foreach (string value in months)
+            {
+                txtOUTPUT.Text += value + "\r\n";
+            }
+
+            //remove an element from the stack 
+            txtOUTPUT.Text += "\r\nRemove last in item of Stack: " + months.Pop().ToString();
+
+            //check if element December is in the stack 
+            if (!months.Contains("Dec")) txtOUTPUT.Text += "\r\n Dec. is not in the Stack!!";
+
+            //view the new top most element of the stack
+            txtOUTPUT.Text +="\r\nRead the top most item of the Stack: " + months.Peek().ToString();
+
+            //empty the stack 
+            months.Clear();
+        }
+
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+           this.Close();
+        }
+    }
+}
